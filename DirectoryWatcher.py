40897a90138fb88c19ce133c484dc611e5eafe32
Federico <Timmy93@users.noreply.github.com>
2021-08-29 16:44:06 +0200

    refactoring

--- DirectoryWatcher.py
+++ DirectoryWatcher.py
@@ -1,65 +1,71 @@
 #!/usr/bin/env python3
 import threading
 import inotify.adapters
-import logging
 import os
 import time
 
-# The main class
-class DirectoryWatcher (threading.Thread):
-   
-	def __init__(self, callbackFunction, logging=None):
+
+class DirectoryWatcher(threading.Thread):
+
+	def __init__(self, callback_function: callable, logging=None):
 		threading.Thread.__init__(self)
 		self.logging = logging
-		self.callbackFunction = callbackFunction
+		self.callbackFunction = callback_function
 		self.logging.info("Created DirectoryWatcher element")
-		
-	def watchThisDirectory(self, directory, events, recursively=False):
+
+	def watch_this_directory(self, directory, events, recursively=False):
 		if not os.path.isdir(directory):
-			self.logging.error("The given directory ["+str(directory)+"] is not a directory")
-			raise Exception("The given directory ["+str(directory)+"] is not a directory")
-			
+			self.logging.error("The given directory [" + str(directory) + "] is not a directory")
+			raise Exception("The given directory [" + str(directory) + "] is not a directory")
+
 		if events:
-			#Consider all events as array
+			# Consider all events as array
 			if type(events) is str:
 				events = [events]
-			self.watchedEvents = events
-			self.logging.info("Monitoring the following events: ["+str(self.watchedEvents)+"]")
+			self.watched_events = events
+			self.logging.info("Monitoring the following events: [" + str(self.watched_events) + "]")
 		else:
-			self.error.info("No events to monitor")
+			self.logging.info("No events to monitor")
 			raise Exception('No events to monitor')
-			
+
 		if recursively:
 			self.notifier = inotify.adapters.InotifyTree(directory)
-			self.logging.info("Starting a recursive monitoring on directory and subdirectory ["+directory+"]")
+			self.logging.info("Starting a recursive monitoring on directory and subdirectory [" + directory + "]")
 		else:
 			self.notifier = inotify.adapters.Inotify()
 			self.notifier.add_watch(directory)
-			self.logging.info("Starting a plain monitoring on directory ["+directory+"]")
-	
+			self.logging.info("Starting a plain monitoring on directory [" + directory + "]")
+
 	def run(self):
-		while(1):
+		while True:
 			try:
 				for event in self.notifier.event_gen():
 					if event is not None:
-						for we in self.watchedEvents:
+						for we in self.watched_events:
 							if we in event[1]:
-								newfile = os.path.join(event[2], event[3])
-								self.logging.info("Registered event ["+str(event[1])+"] for file ["+newfile+"]")
+								new_file = os.path.join(event[2], event[3])
+								self.logging.info(
+									"Registered event [" + str(event[1]) + "] for file [" + new_file + "]")
 								try:
-									while not self.stableSize(newfile):
-										self.logging.info("Still writing on file ["+newfile+"]")
-									self.callbackFunction(newfile)
+									while not self.stable_size(new_file):
+										self.logging.info("Still writing on file [" + new_file + "]")
+									self.callbackFunction(new_file)
 								except FileNotFoundError as err:
-									self.logging.warning("File missing ["+newfile+"] - Skipping callback ["+str(err)+"]")
+									self.logging.warning(
+										"File missing [" + new_file + "] - Skipping callback [" + str(err) + "]")
 				self.logging.warning("DirectoryWatcher stopped checking directory")
 			except RuntimeError as err:
 				self.logging.exception("Thread is dead for a Runtime error")
 			except Exception as err:
 				self.logging.exception("Unexpected thread death")
-	
-	#Check if the file is still being written
-	def stableSize(self, myFile):
-		initialSize = os.path.getsize(myFile)
+
+	@staticmethod
+	def stable_size(my_file):
+		"""
+		Check if the file is still being written
+		:param my_file: The file to control
+		:return: True if the file has not changed size during last second. False otherwise
+		"""
+		initial_size = os.path.getsize(my_file)
 		time.sleep(1)
-		return os.path.getsize(myFile) == initialSize
+		return os.path.getsize(my_file) == initial_size
